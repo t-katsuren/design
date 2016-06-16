cd8b79c659842f8ca4e30eef6ec80bc0d8c1c840
t-katsuren <t-katsuren@occ.co.jp>
2016-06-16 17:32:43 +0900

    add builder

---
+++ src/builder/self/Builder.java
@@ -0,0 +1,13 @@
+package builder.self;
+
+public interface Builder {
+
+	public abstract void makeTitle(String title);
+
+	public abstract void makeString(String str);
+
+	public abstract void makeItems(String[] items);
+
+	public abstract void close();
+
+}
---
+++ src/builder/self/Main.java
@@ -0,0 +1,21 @@
+package builder.self;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		TextBuilder textBuilder = new TextBuilder();
+		Director textDirector = new Director(textBuilder);
+		textDirector.construct();
+		String result = textBuilder.getResult();
+		System.out.println(result);
+
+		HTMLBuilder htmlBuilder = new HTMLBuilder();
+		Director htmlDirector = new Director(htmlBuilder);
+		htmlDirector.construct();
+		String filename = htmlBuilder.getResult();
+		System.out.println(filename + "が作成されました。");
+
+	}
+
+}
